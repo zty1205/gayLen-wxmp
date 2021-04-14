a5677dc72f0d2ef9e5687994a125bbc5419af71c
张腾跃 <tengyuezhang@91jkys.com>
2021-04-14 15:45:16 +0800

    media

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+    const performance = wx.getPerformance()
+    const observer = performance.createObserver((entryList) => {
+      console.log(entryList.getEntries())
+    })
+    observer.observe({
+      entryTypes: ['navigation', 'render', 'script']
     })
   },
   globalData: {
